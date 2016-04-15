a65c75221763c79d1df249667ad777f8051649de
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 00:54:41 +0200

    Power strip thingie and start of placement

---
+++ Assets/Scripts/PlayerPlacementCalculator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPlacementCalculator : MonoBehaviour {
+    private GameObject[] players;
+	// Use this for initialization
+	void Start () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
--- Assets/Scripts/PowerDistribution.cs
+++ Assets/Scripts/PowerDistribution.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PowerDistribution : MonoBehaviour {
+    public List<GameObject> FirstPlacedPowerPool = new List<GameObject>();
+    public List<GameObject> SecondPlacedPowerPool = new List<GameObject>();
+    public List<GameObject> ThirdPlacedPowerPool = new List<GameObject>();
+    public List<GameObject> FourthPlacedPowerPool = new List<GameObject>();
 
-	// Use this for initialization
-	void Start () {
-	
+    private List<GameObject> playersVisited = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider unit)
+    {
+        if (unit.CompareTag("Player"))
+        {           
+            bool givePower = true;
+            foreach (GameObject player  in playersVisited)
+            {
+                if (player == unit.gameObject)
+                {
+                    givePower = false;
+                }
+            }
+            if (givePower)
+            {
+                // Give power here!
+                Debug.Log("Give player power");
+            }
+        }
+    }
 }
