291d9d01dbd17e8c3cc9f0dea9a275fe4ef519ed
Yooric2 <yooric90@hotmail.com>
2016-04-16 00:56:29 +0200

    Lagt till pupHandler o Pupspeedscript
    inget är ngåt vettigt osm fungerar bara skit

---
+++ Assets/PUpSpeedScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PUpSpeedScript : MonoBehaviour {
+
+    public void FireMe()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PowerUpHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpHandler : MonoBehaviour {
+    int powerUpInt = 0; // 0 = None, 1 = SpeedUp, 2 = SpeedDown, 3 = SpeedRocket, 4 = SlowRocket.
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
