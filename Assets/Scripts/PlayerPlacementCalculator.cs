7d17bf21778ee50ad366e49f278d84c06e9c4235
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 01:27:17 +0200

    Placement fixed

--- Assets/Scripts/PlayerPlacementCalculator.cs
+++ Assets/Scripts/PlayerPlacementCalculator.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class PlayerPlacementCalculator : MonoBehaviour {
     private GameObject[] players;
+    private List<GameObject> p = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
         players = GameObject.FindGameObjectsWithTag("Player");
-	}
+        players.OrderBy(platform => platform.transform.position.x);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        players = players.OrderBy(platform => platform.transform.position.x).ToArray();
+        Debug.Log(players[2].transform.position.x);
+    }
 }
