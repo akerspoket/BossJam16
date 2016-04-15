5f01c01093a797eaa3cdce10eeafc5939bfba6c2
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-15 22:26:42 +0200

    Camera and constraints added to dummy scene

---
+++ Assets/BullShit/CameraFocalPlacement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraFocalPlacement : MonoBehaviour {
+    private GameObject[] players;
+	// Use this for initialization
+	void Start () {
+        players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int length = players.GetLength(0);
+        Vector3 position = Vector3.zero;
+        for (int i = 0; i < length; i++)
+        {
+            position += players[i].transform.position;
+        }
+        position /= length;
+        position.y = transform.position.y;
+        transform.position = position;
+	}
+}
---
+++ Assets/BullShit/ConstantMovementForward.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantMovementForward : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = transform.right * speed;
+	}
+}
