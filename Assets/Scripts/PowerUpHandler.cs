5c92d42c4413097253b19d9a42ffe0701b0dacce
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 16:13:56 +0200

    Hud for powerups with temporary images

--- Assets/Scripts/PowerUpHandler.cs
+++ Assets/Scripts/PowerUpHandler.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class PowerUpHandler : MonoBehaviour {
     int powerUpInt = 0; // 0 = None, 1 = SpeedUp, 2 = SpeedDown, 3 = SpeedRocket, 4 = SlowRocket.
@@ -7,10 +9,12 @@ public class PowerUpHandler : MonoBehaviour {
     public float speedDown = 1;
     public GameObject speedRocket;
     public GameObject slowRocket;
+    public Image pupDisplay;
+    public List<Sprite> powerUpSprites = new List<Sprite>();
     // Use this for initialization
     void Start ()
     {
-        powerUpInt = 3;
+        SetPowerUp(3);
     }
 	
 	// Update is called once per frame
@@ -21,24 +25,22 @@ public class PowerUpHandler : MonoBehaviour {
     public void SetPowerUp(int p_powerUpId)
     {
         powerUpInt = p_powerUpId;
+        pupDisplay.GetComponent<Image>().sprite = powerUpSprites[powerUpInt];
     }
     public void FirePowerUp()
     {
         if(powerUpInt ==0)
         {
             //inte ett smack
-            powerUpInt = 0;
         }
         else if(powerUpInt==1)
         {
             GetComponent<Rigidbody>().AddForce(new Vector3(speedUp, 0.0f, 0.0f));
-            powerUpInt = 0;
             // Speedup
         }
         else if(powerUpInt==2)
         {
             GetComponent<Rigidbody>().AddForce(new Vector3(-speedDown, 0.0f, 0.0f));
-            powerUpInt = 0;
             //SpeedDown
         }
         else if(powerUpInt==3)
@@ -47,7 +49,6 @@ public class PowerUpHandler : MonoBehaviour {
             t_speedRocket.transform.position = transform.position + (transform.forward);
             t_speedRocket.transform.GetComponent<Rigidbody>().AddForce(transform.forward);
             t_speedRocket.GetComponent<RocketHitScript>().SetIgnoreTarget(this.gameObject);
-            powerUpInt = 0;
             //SpeedRocket
         }
         else if(powerUpInt==4)
@@ -55,8 +56,8 @@ public class PowerUpHandler : MonoBehaviour {
             GameObject t_slowRocket = Instantiate(slowRocket);
             t_slowRocket.transform.position = transform.position + (transform.forward * 5);
             t_slowRocket.transform.GetComponent<Rigidbody>().AddForce(transform.forward * 100);
-            powerUpInt = 0;
             //SlowRocket
         }
+        SetPowerUp(0);
     }
 }
