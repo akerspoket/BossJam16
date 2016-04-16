a18f4c282724a9a3893c5baca8c1f35a60f34233
Yooric2 <yooric90@hotmail.com>
2016-04-16 13:51:26 +0200

    added goaltrigger
    asd

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+    private int counter = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(counter == 3)
+            {
+                //Debug.Log("Jo fan du vann!WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+                GameObject[] list = GameObject.FindGameObjectsWithTag("Player");
+                int listLength = list.Length;
+                //Debug.Log(listLength);
+                for (int i = 0; i < listLength; i++)
+                {
+                    if(list[i]==other)
+                    {
+                        //sätt grabben uppe på pallfan
+                        //Debug.Log("Denna vann!!");
+                    }
+                    else
+                    {
+                        //Debug.Log("Denna vann inte=(!");
+                        // sätt förloraren där nere.
+                    }
+                }
+            }
+            else if(counter !=3)
+            {
+                ++counter;
+            }
+        }
+
+    }
+}
