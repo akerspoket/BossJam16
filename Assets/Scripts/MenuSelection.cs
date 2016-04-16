eee296c58ccb20ff5a14cf76f10a34d08f389834
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 20:16:45 +0200

    Main menu fix

--- Assets/Scripts/MenuSelection.cs
+++ Assets/Scripts/MenuSelection.cs
@@ -12,11 +12,23 @@ public class MenuSelection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        float y = Input.GetAxis("Vertical");
+        if (y > 0.5)
+        {
+            ChangeSelection(currentlySelected - 1);
+        }
+        else if (y < -0.5)
+        {
+            ChangeSelection(currentlySelected + 1);
+        }
 	}
 
     private void ChangeSelection(int newSelection)
     {
+        if (newSelection < 0 || newSelection > buttons.Count - 1)
+        {
+            return;
+        }
         currentlySelected = newSelection;
         transform.position = buttons[currentlySelected].transform.position;
     }
