e91bdb494bf1b509149fcdb9921bff7adccdf4a8
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 18:46:00 +0200

    dded leader indication and correct hud

--- Assets/Scripts/Placement.cs
+++ Assets/Scripts/Placement.cs
@@ -3,4 +3,15 @@ using System.Collections;
 
 public class Placement : MonoBehaviour {
     public int place = 0;
+    void Update()
+    {
+        if (place == 3)
+        {
+            GetComponentInChildren<Light>().enabled = true;
+        }
+        else
+        {
+            GetComponentInChildren<Light>().enabled = false;
+        }
+    }
 }
