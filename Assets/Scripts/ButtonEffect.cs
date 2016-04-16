a504a8cbeaf71e53bb27e06018de188a7d3e54af
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 19:15:53 +0200

    Filled menu, almost done

---
+++ Assets/Scripts/ButtonEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonEffect : MonoBehaviour {
+    public string sceneToLoad;
+    public bool Exit = false;
+    // Use this for initialization
+    void LoadTheScene()
+    {
+        if (Exit)
+        {
+            Application.Quit();
+        }
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
---
+++ Assets/Scripts/MenuSelection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuSelection : MonoBehaviour {
+    public List<GameObject> buttons = new List<GameObject>();
+    private int currentlySelected;
+	// Use this for initialization
+	void Start () {
+        ChangeSelection(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private void ChangeSelection(int newSelection)
+    {
+        currentlySelected = newSelection;
+        transform.position = buttons[currentlySelected].transform.position;
+    }
+
+}
