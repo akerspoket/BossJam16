95196619bace4ec4bfcaf28016aa5295fa3ed31d
Slowey <lucas.holmqvist@gmail.com>
2016-04-16 14:17:58 +0200

    Fixed rocketsscripts

--- Assets/RocketHitScript.cs
+++ Assets/RocketHitScript.cs
@@ -4,17 +4,17 @@ using System.Collections;
 public class RocketHitScript : MonoBehaviour {
     public float force;
     private GameObject ignoredPlayer;
+    public float heatSeekingFactor;
 	// Use this for initialization
 	void Start () {
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         GameObject[] list = GameObject.FindGameObjectsWithTag("Player");
         int listLength = list.Length;
-        Vector3 ClosestTarget = new Vector3(0,0,0);
         float ClosestDistance = 99999999999999999.0f;
+        Vector3 vecBetweenMeAndTarget = new Vector3(0, 0, 0);
         GameObject ClosestObject;
         Vector3 myPosition = transform.position;
         for (int i = 0; i < listLength; i++)
@@ -27,16 +27,15 @@ public class RocketHitScript : MonoBehaviour {
                 {
                     ClosestDistance = lengthBetweenPlayers;
                     ClosestObject = list[i];
-
+                    vecBetweenMeAndTarget = vecBetweenMeAndPlayer.normalized;
                 }
             }
         }
-
-
+        transform.GetComponent<Rigidbody>().AddForce(vecBetweenMeAndTarget * heatSeekingFactor);
     }
     void OnTriggerEnter(Collider Player)
     {
-        if (Player.CompareTag("Player"))
+        if (Player.CompareTag("Player") && Player.gameObject != ignoredPlayer)
         {
             //Debug.Log("PlayerCollided");
             Player.transform.GetComponent<Rigidbody>().AddForce(new Vector3(force, 0.0f, 0.0f));
