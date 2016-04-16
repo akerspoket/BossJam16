66085fa3ca1a4875138b09a0174f36e6655fa0c5
Konrad Öhman <konrad_o_92@hotmail.com>
2016-04-16 20:58:54 +0200

    Acctually done dummy scene

--- Assets/Scripts/DummyChangeToFirstScene.cs
+++ Assets/Scripts/DummyChangeToFirstScene.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class DummyChangeToFirstScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        SceneManager.LoadScene("MainMenu");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
